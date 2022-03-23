65da06e254c7ac38289749593ff8d11d1d2148be
Tim Schäfer <ts+code@rcmd.org>
2022-03-23 09:30:16 +0100

    improve reprocess after edits script

--- cross-sectional/reprocess_after_manual_edits_parallel.bash
+++ cross-sectional/reprocess_after_manual_edits_parallel.bash
@@ -13,6 +13,10 @@ SCRIPT_FOR_SINGLE_SUBJECT="reprocess_after_manual_edits_single_subject.bash"
 SUBJECTS_FILE=$1
 if [ -z "$SUBJECTS_FILE" ]; then                              # name of subjects file that will be generated (and used) by this script
   echo "USAGE: $0 <subjects_file> [<num_cores>]"
+  echo " <subjects_files>: str, path to text file containing one subject dir name after edits per line."
+  echo "                   I.e., the names must end with '_gm' or '_wm'. Other subjects will be ignored."
+  echo "Note: The SUBJECTS_DIR environment variable must also be set properly."
+  echo "      SUBJECTS_DIR currently points at '$SUBJECTS_DIR'."
   exit 1
 fi
 
@@ -38,12 +42,31 @@ NUM_SUBJECTS=$(cat ${SUBJECTS_FILE} | wc -l | tr -d '[:space:]')
 echo "$APPTAG Running recon-all reprocessing after edits in parallel for ${NUM_SUBJECTS} subjects from file $SUBJECTS_FILE using $NUM_CORES_TO_USE cores."
 
 SUBJECTS=$(cat ${SUBJECTS_FILE} | tr '\n' ' ')
+
+num_subects_will_be_handled=0
+num_subects_will_be_skipped=0
+
+# Some sanity checks.
 for SUBJECT_ID in $SUBJECTS; do
+  # check whether subject dir exists.
     if [ ! -d "${SUBJECTS_DIR}"/"${SUBJECT_ID}" ]; then
-        echo "Missing data dir for subject '$SUBJECT_ID' under SUBJECTS_DIR '$SUBJECTS_DIR'. Exiting."
+        echo "$APPTAG Missing data dir for subject '$SUBJECT_ID' under SUBJECTS_DIR '$SUBJECTS_DIR'. Exiting."
         exit 1
     fi
+    # Check whether subject names have the correct patterns.
+    if [[ "${SUBJECT_ID}" == "*gm" -o "${SUBJECT_ID}" == "*wm" ]]; then
+        num_subects_will_be_handled=$((num_subects_will_be_handled+1))
+    else
+        num_subects_will_be_skipped=$((num_subects_will_be_skipped+1))
+        echo "$APPTAG WARNING: Subject '${SUBJECT_ID}' does not follow naming convention ('_gm' / '_wm' suffix) and will be skipped."
+    fi
 done
+
+if [ $num_subects_will_be_handled -eq 0 ]; then
+    echo "$APPTAG None of the $NUM_SUBJECTS subjects in the subjects file adhere to the required naming convention ('_gm' / '_wm' suffix). Would not do anything. Stopping now."
+fi
+
+echo "$APPTAG Out of the $NUM_SUBJECTS subjects in the subjects file, $num_subects_will_be_handled will be re-processed and $num_subects_will_be_skipped will be ignored because they do not adhere to the required naming convention ('_gm' / '_wm' suffix)."
 
 EXEC_PATH_OF_THIS_SCRIPT=$(dirname $0)
 PATH_TO_SSCRIPT="$EXEC_PATH_OF_THIS_SCRIPT/$SCRIPT_FOR_SINGLE_SUBJECT"
--- cross-sectional/reprocess_after_manual_edits_single_subject.bash
+++ cross-sectional/reprocess_after_manual_edits_single_subject.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## reprocess_after_manual_edits_single_subject.bash -- reprocess a single subject in FreeSurfer after edits
 ##
-## There should be no need to run this script. It is called by the parallel wrapper script instead.
+## There should be no need to run this script. It is called by the parallel wrapper script 'reprocess_after_manual_edits_parallel.bash' for each subject instead.
 ##
 ## Usage: reprocess_after_manual_edits_single_subject.bash <subject_id>, where the ID must end with '_gm' or '_wm'
 
@@ -11,15 +11,18 @@ SUBJECT_ID="$1"
 
 if [ -z "${SUBJECT_ID}" ]; then
     echo "USAGE: $0 <subject_id>"
+    echo " <subject_id>: str, the subject directory name afte edits, must end with '_gm' or '_wm'."
+    echo "Note: The SUBJECTS_DIR environment variable must be set properly."
+    echo "      SUBJECTS_DIR currently points at '$SUBJECTS_DIR'."
     exit 1
 fi
 
-if [[ "${SUBJECT_ID}" == *gm ]]; then
+if [[ "${SUBJECT_ID}" == "*gm" ]]; then
     echo "$APPTAG Reprocessing subject $SUBJECT_ID after only gray matter edits..."
     recon-all -autorecon-pial -sd `pwd` -subjid $SUBJECT_ID -no-isrunning && recon-all -sd "${SUBJECTS_DIR}" -subjid $SUBJECT_ID -qcache
-elif [[ "${SUBJECT_ID}" == *wm ]]; then
+elif [[ "${SUBJECT_ID}" == "*wm" ]]; then
     echo "$APPTAG Reprocessing subject $SUBJECT_ID after white matter edits..."
     recon-all -autorecon2-wm -autorecon3 -sd `pwd` -subjid $SUBJECT_ID -no-isrunning && recon-all -sd "${SUBJECTS_DIR}" -subjid $SUBJECT_ID -qcache
 else
-  echo "WARNING: The name of subject '${SUBJECT_ID}' does not match edited directory names (_gm, _wm), skipping."
+  echo "$APPTAG WARNING: The name of subject '${SUBJECT_ID}' does not match edited directory names (_gm, _wm), skipping."
 fi
