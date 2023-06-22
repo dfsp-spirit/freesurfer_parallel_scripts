06ec24a556ab88fa9a8257e96c04bd9a72520081
Tim Schäfer <ts+code@rcmd.org>
2023-06-22 14:25:59 +0200

    NEW: Add parallel version of anatomical stats script

---
+++ tools/parallel_anatomical_stats_inner.bash
@@ -0,0 +1,89 @@
+#!/bin/bash
+# Compute anatomical stats for a one or more custom measures of a single subject. Can be run in parallel with wrapper 'parallel_anatomical_stats_outer.bash'.
+# This script runs sequentially for one subject, and should be called via the parallel wrapper 'parallel_anatomical_stats_outer.bash' to run in parallel for multiple subjects.
+# Written by Tim, 2023-06-22
+
+APPTAG="[RUN_ANAT_STATS_SUBJECT]"
+
+
+if [ -z "$1" ]; then
+  echo "$APPTAG ERROR: Arguments missing."
+  echo "$APPTAG Usage: $0 <subject>"
+  echo "$APPTAG Note that the environment variable SUBJECTS_DIR must also be set correctly."
+  exit 1
+else
+  SUBJECT="$1"
+fi
+
+
+if [ -z "$SUBJECTS_DIR" ]; then
+    echo "$APPTAG ERROR: Environment variable SUBJECTS_DIR not set, you must export it."
+    exit 1
+fi
+
+SUBJECTS_FILE="${SUBJECTS_DIR}/subjects.txt"
+
+echo "$APPTAG Using subjects file from '${SUBJECTS_FILE}'"
+
+ALL_SUBJECT_IDS=$(cat "${SUBJECTS_FILE}" | tr '\n' ' ')
+SUBJECT_COUNT=$(echo "$ALL_SUBJECT_IDS" | wc -w | tr -d '[:space:]')
+
+MEASURES="thickness area volume"
+HEMIS="lh rh"
+ATLAS="aparc.a2009s"
+
+do_exit_on_missing_input="no" # only for missing input files. set to "yes" or "no".
+do_exit_on_write_error="no" # only for write errors. we still exit if input files are missing. set to "yes" or "no".
+
+for HEMI in $HEMIS; do
+    for MEASURE in ${MEASURES}; do
+        OUTPUT_FILE="${SUBJECTS_DIR}/${SUBJECT}/stats/${HEMI}.${ATLAS}.${MEASURE}.stats"
+
+        # Check for input files, as mris_anatomical_stats still does (useless) stuff if they are missing
+        INPUT_FILE_ATLAS="${SUBJECTS_DIR}/${SUBJECT}/label/${HEMI}.${ATLAS}.annot"
+        if [ ! -f "${INPUT_FILE_ATLAS}" ]; then
+            if [ $do_exit_on_missing_input = "yes" ]; then
+                echo "$APPTAG ERROR: Subject $SUBJECT: Input atlas file '${INPUT_FILE_ATLAS}' for atlas '${ATLAS}' hemi '${HEMI}' missing. Exiting."
+                exit 1
+            else
+                echo "$APPTAG ERROR: Subject $SUBJECT: Input atlas file '${INPUT_FILE_ATLAS}' for atlas '${ATLAS}' hemi '${HEMI}' missing. Skipping."
+                continue
+            fi
+        fi
+        INPUT_FILE_MEASURE="${SUBJECTS_DIR}/${SUBJECT}/surf/${HEMI}.${MEASURE}"
+        if [ ! -f "${INPUT_FILE_MEASURE}" ]; then
+            if [ $do_exit_on_missing_input = "yes" ]; then
+                echo "$APPTAG ERROR: Subject $SUBJECT: Input measure file (per-vertex data overlay) '${INPUT_FILE_MEASURE}' for measure '${MEASURE}' hemi '${HEMI}' missing. Exiting."
+                exit 1
+            else
+                echo "$APPTAG ERROR: Subject $SUBJECT: Input measure file (per-vertex data overlay) '${INPUT_FILE_MEASURE}' for measure '${MEASURE}' hemi '${HEMI}' missing. Skipping."
+                continue
+            fi
+        fi
+
+        CMD="mris_anatomical_stats -a ${ATLAS}.annot -t ${SUBJECTS_DIR}/${SUBJECT}/surf/${HEMI}.${MEASURE} -f ${OUTPUT_FILE} ${SUBJECT} ${HEMI}"
+        echo "$APPTAG ***Running for subject $SUBJECT hemi $HEMI measure $MEASURE with command: '$CMD'"
+        $CMD
+        if [ $? -eq 1 ]; then
+            if [ "$do_exit_on_write_error" = "yes" ]; then
+                echo "$APPTAG Call to mris_anatomical_stats failed (see above). Exiting."
+                exit 1
+            else
+                echo "$APPTAG Call to mris_anatomical_stats failed (see above). No output file written for subject $SUBJECT hemi $HEMI measure $MEASURE atlas $ATLAS."
+                continue
+            fi
+        fi
+
+        echo "$APPTAG ***Done for subject $SUBJECT hemi $HEMI measure $MEASURE. Check output file '${OUTPUT_FILE}'."
+        if [ ! -f "$OUTPUT_FILE" ]; then
+            if [ "$do_exit_on_write_error" = "yes" ]; then
+                echo "$APPTAG ERROR: Expected output file '${OUTPUT_FILE}' for subject $SUBJECT hemi $HEMI measure $MEASURE atlas $ATLAS missing. Check output above for errors. Exiting."
+                exit 1
+            else
+                echo "$APPTAG ERROR: Expected output file '${OUTPUT_FILE}' for subject $SUBJECT hemi $HEMI measure $MEASURE atlas $ATLAS missing. Check output above for errors."
+                continue
+            fi
+        fi
+    done
+
+done
---
+++ tools/parallel_anatomical_stats_outer.bash
@@ -0,0 +1,71 @@
+#!/bin/bash
+# parallel_anatomical_stats_outer.bash -- compute anatomical stats in parallel over a number of subjects.
+#
+# HOW TO TO USE THIS script
+#  1) Copy it wherever you like and make sure it is executable
+#  2) Set your SUBJECTS_DIR environment variable and change into your SUBJECTS_DIR. You should have a subjects.txt file in there.
+#  3) Run: `path/to/parallel_anatomical_stats_outer.bash subjects.txt`
+#
+# Written by Tim, 2023-06-22
+#
+
+APPTAG="[PAR_ANAT_STATS]"
+
+##### General settings #####
+
+# Number of consecutive GNU Parallel jobs. Note that 0 for 'as many as possible'. Maybe set something a little bit less than the number of cores of your machine if you want to do something else while it runs.
+# See 'man parallel' for details. On MacOS, try `sysctl -n hw.ncpu` to find the number of cores you have.
+NUM_CONSECUTIVE_JOBS=7
+###### End of job settings #####
+
+
+
+## check some stuff
+if [ -z "${SUBJECTS_DIR}" ]; then
+    echo "$APPTAG WARNING: Environment variable SUBJECTS_DIR not set."
+fi
+
+if [ -d "${SUBJECTS_DIR}/bert" ]; then
+    echo "$APPTAG WARNING: Environment variable SUBJECTS_DIR seems to point at the subjects dir of the FreeSurfer installation: '${SUBJECTS_DIR}'. Configure it to point at your data!"
+fi
+
+if [ -n "$1" ]; then
+    SUBJECTS_FILE="$1"
+    ## Check for given subjects file.
+    if [ ! -f "$SUBJECTS_FILE" ]; then
+        echo "$APPTAG ERROR: Subjects file '$SUBJECTS_FILE' not found."
+        exit 1
+    fi
+else
+    echo "$APPTAG ERROR: Must specify subjects_file. Exiting."
+    echo "$APPTAG Usage: $0 <subjects_file>"
+    exit 1
+fi
+
+
+SUBJECTS=$(cat "${SUBJECTS_FILE}" | tr '\n' ' ')
+SUBJECT_COUNT=$(echo "${SUBJECTS}" | wc -w | tr -d '[:space:]')
+
+
+echo "$APPTAG Parallelizing over the ${SUBJECT_COUNT} subjects in file '${SUBJECTS_FILE}' using $NUM_CONSECUTIVE_JOBS threads."
+
+# We can check already whether the subjects exist.
+for SUBJECT in $SUBJECTS; do
+  if [ ! -d "${SUBJECTS_DIR}/${SUBJECT}" ]; then
+    echo "$APPTAG ERROR: Directory for subject '${SUBJECT}' not found in SUBJECTS_DIR '${SUBJECTS_DIR}'. Exiting."
+    exit 1
+  fi
+done
+
+
+
+EXEC_PATH_OF_THIS_SCRIPT=$(dirname $0)
+CARGO_SCRIPT="${EXEC_PATH_OF_THIS_SCRIPT}/parallel_anatomical_stats_inner.bash"
+
+if [ ! -x "${CARGO_SCRIPT}" ]; then
+    echo "ERROR: Cargo script not found or not executable at '${CARGO_SCRIPT}'."
+    exit 1
+fi
+
+DATE_TAG=$(date '+%Y-%m-%d_%H-%M-%S')
+echo ${SUBJECTS} | tr ' ' '\n' | parallel --jobs ${NUM_CONSECUTIVE_JOBS} --workdir . --joblog LOGFILE_PARALLEL_ANATSTATS_${DATE_TAG}.txt "${CARGO_SCRIPT} {}"
