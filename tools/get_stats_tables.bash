e970d3ba6a399e760e7ffe180d69d053e16f786b
Tim Schäfer <ts+code@rcmd.org>
2023-06-23 06:59:39 +0200

    NEW: support different atlas

--- tools/get_stats_tables.bash
+++ tools/get_stats_tables.bash
@@ -4,22 +4,37 @@
 #
 # Note that for FS7, aparcstats is a shell script and must thus not be run with Python anymore,
 # it is a wrapper that calls its own Python interpreter in the FREESURFER_HOME.
+#
+#
+# Usage: path/to/get_stats_tables.bash <subjects_dir> <subjects_file>
+#
+# One dies not need to export env var for subjects dir, this script does it.
+
+
+
 
 ### Settings ###
 
+do_aseg_stats="no"   # set to "yes" or "no"
+atlas="aparc.a2009s" # set to "aparc" or "aparc.a2009s" (or a custom atlas if you have already applied it to subjects)
+measures="thickness area volume"   # the per-vertex descriptor files to use (aka "surface overlays")
+
+### End of Settings ###
+
+
+
 # auto-detect FS6 vs FS7.
 if [ -z "$FREESURFER_HOME" ]; then
-    echo "ERROR: Environment variable FREESURFER_HOME not set, you must export it and point it to your FreeSurfer installation."
+    echo "$apptag ERROR: Environment variable FREESURFER_HOME not set, you must export it and point it to your FreeSurfer installation."
     exit 1
 fi
-grep_res = $(fgrep 'x86_64-stable-pub-v6.' $FREESURFER_HOME/build-stamp.txt)
+grep_res=$(fgrep 'x86_64-stable-pub-v6.' $FREESURFER_HOME/build-stamp.txt)
 if [ -n "${grep_res}" ]; then
     is_fs7="no"
 else
     is_fs7="yes"
 fi
 
-### End of settings ###
 
 apptag="[GET_STATS_TBL]"
 
@@ -31,13 +46,17 @@ fi
 
 #is_fs7="no" # uncomment this line to force FS6 mode.
 
-### Settings ###
+
+
+### Path Settings ###
+
 python2_bin=$(which python2) # Only needed for FS6.
 #python2_bin=/usr/local/bin/python2
 
-###
 aparcstats2table_bin=$(which aparcstats2table)
 asegstats2table_bin=$(which asegstats2table)
+
+
 
 ### Command line args ###
 subjects_dir=$1
@@ -46,29 +65,29 @@ subjects_file=$2
 #### sanity checks
 
 if [ -z "$subjects_file" ]; then
-    echo "USAGE: $0 <subjects_dir> <subjects_file>"
+    echo "$apptag USAGE: $0 <subjects_dir> <subjects_file>"
     exit 1
 fi
 
 if [ ! -d $subjects_dir ];  then
-    echo "ERROR: Subjects dir '$subjects_dir' does not exist."
+    echo "$apptag ERROR: Subjects dir '$subjects_dir' does not exist."
     exit 1
 fi
 
 if [ ! -f $subjects_file ];  then
-    echo "ERROR: Subjects file '$subjects_file' does not exist."
+    echo "$apptag ERROR: Subjects file '$subjects_file' does not exist."
     exit 1
 fi
 
 if [ "$is_fs7" != "yes" ]; then
 
     if [ -z "$python2_bin" ]; then
-        echo "ERROR: Could not autodetect path to python2 binary, please adapt setting 'python2_bin' in this script."
+        echo "$apptag ERROR: Could not autodetect path to python2 binary, please adapt setting 'python2_bin' in this script."
         exit 1
     fi
 
     if [ ! -x "$python2_bin" ]; then
-        echo "ERROR: Cannot execute python2 binary at '$python2_bin'"
+        echo "$apptag ERROR: Cannot execute python2 binary at '$python2_bin'"
         exit 1
     fi
 fi
@@ -80,27 +99,31 @@ export SUBJECTS_DIR="${subjects_dir}"
 subjects=$(cat $subjects_file | tr '\n' ' ')
 num_subjects=$(echo "${subjects}" | wc -w | tr -d '[:space:]')
 
-echo "Getting stats for $num_subjects subjects."
+echo "$apptag Gathering stats for atlas '$atlas' for $num_subjects subjects, measures: $measures."
 
 for hemi in lh rh; do
-    for measure in thickness area volume; do # Feel free to add more measures here, see the help of aparcstats2table for options.
-        aparc_output_table="${hemi}.aparc_table_${measure}.tsv"
+    echo "$apptag Handling hemisphere $hemi."
+    for measure in $measures; do # Feel free to add more measures here, see the help of aparcstats2table for options.
+        aparc_output_table="${hemi}.${atlas}_table_${measure}.tsv"
 	# You many want to add more command line options to the call in the next line. E.g., '--skip' or '--common-parcs' may come in handy.
         if [ "$is_fs7" = "yes" ]; then
-            $aparcstats2table_bin --subjectsfile $subjects_file --meas $measure --hemi $hemi -t $aparc_output_table && echo " * output file '$aparc_output_table' written."
+            $aparcstats2table_bin --subjectsfile $subjects_file --parc $atlas --meas $measure --hemi $hemi -t $aparc_output_table && echo " * output file '$aparc_output_table' written."
         else
-            $python2_bin $aparcstats2table_bin --subjectsfile $subjects_file --meas $measure --hemi $hemi -t $aparc_output_table && echo " * output file '$aparc_output_table' written."
+            $python2_bin $aparcstats2table_bin --subjectsfile $subjects_file --parc $atlas --meas $measure --hemi $hemi -t $aparc_output_table && echo " * output file '$aparc_output_table' written."
         fi
     done
 
-    aseg_output_table="aseg_table.tsv"
-    if [ "$is_fs7" = "yes" ]; then
-        $asegstats2table_bin --subjectsfile $subjects_file -t $aseg_output_table && echo " * output file '$aseg_output_table' written."
-    else
-        $python2_bin $asegstats2table_bin --subjectsfile $subjects_file -t $aseg_output_table && echo " * output file '$aseg_output_table' written."
+    if [ "$do_aseg_stats" = "yes" ]; then
+        echo "$apptag Gathering aseg stats."
+	    aseg_output_table="aseg_table.tsv"
+	    if [ "$is_fs7" = "yes" ]; then
+	        $asegstats2table_bin --subjectsfile $subjects_file --parc $atlas -t $aseg_output_table && echo " * output file '$aseg_output_table' written."
+	    else
+        	$python2_bin $asegstats2table_bin --subjectsfile --parc $atlas $subjects_file -t $aseg_output_table && echo " * output file '$aseg_output_table' written."
+	    fi
     fi
 
 done
 
-echo "All done, exiting."
+echo "$apptag All done, exiting."
 exit 0
