FIX: fix job count computation

slurm/subjects_txt_to_jobarray_config.py
@@ -44,36 +44,49 @@ def subjects_txt_to_jobarray_config(subjects_file : str, num_parallel_jobs_max :
     return res
 
 
-def subjects_txt_to_jobarray_config_sf(subjects_file : str, num_parallel_jobs_max : Union[int, None] = None):
+def split_subjects_txt_into_chunks(subjects_file : str, num_parallel_jobs : Union[int, None] = None):
 
     subjects : List[str] = read_subjects_file(subjects_file)
 
     res = "ArrayTaskID NumSubjects Subjects\n"  # header row
 
-    if num_parallel_jobs_max is None:
-        num_parallel_jobs_max = len(subjects)
-
     num_subjects = len(subjects)
-    num_subjects_max_per_jobs = math.ceil(num_subjects / num_parallel_jobs_max)
-    num_in_last_job = num_subjects % num_parallel_jobs_max
-    
 
-    num_subjects_per_job = [num_subjects_max_per_jobs] * num_parallel_jobs_max 
-    if num_in_last_job > 0:
-        num_subjects_per_job[-1] = num_in_last_job  
+    if num_parallel_jobs is None:
+        num_parallel_jobs = num_subjects
+
+    if num_subjects < num_parallel_jobs:
+        num_parallel_jobs = num_subjects
+        print(f"Note: Received only {num_subjects} subjects, so will only run {num_parallel_jobs} jobs (even if {num_parallel_jobs} are allowed).")
+
+
+    num_subjects_per_job = math.ceil(num_subjects / num_parallel_jobs)
+    num_in_last_job = num_subjects_per_job if num_subjects % num_subjects_per_job == 0 else num_subjects % num_subjects_per_job
+    num_jobs_required = math.ceil(num_subjects / num_subjects_per_job)
+    if num_jobs_required < num_parallel_jobs:
+        print(f"Will only use {num_jobs_required} out of {num_parallel_jobs} possible jobs.")
+        num_parallel_jobs = num_jobs_required
+    print(f"Received {num_subjects} subjects and max jobs = {num_parallel_jobs}")
+    print(f"Using {num_subjects_per_job} subjects per job, last job will contain {num_in_last_job} subjects.")
+
+
+    num_subjects_per_job = [num_subjects_per_job] * num_parallel_jobs
+    if num_in_last_job != num_subjects_per_job:
+        num_subjects_per_job[-1] = num_in_last_job
+    print(f'Subjects per job ({len(num_subjects_per_job)} entries): {num_subjects_per_job} ')
 
     subjects = np.array(subjects)
     current_idx = 0
     for jobidx, subjects_count_current_job in enumerate(num_subjects_per_job):
         start_idx = current_idx
         end_idx = start_idx + subjects_count_current_job
-        print(f"At job {jobidx+1} (index {jobidx}) of {num_parallel_jobs_max}: using subject indices {start_idx} to {end_idx}.")
+        print(f"At job {jobidx+1} (index {jobidx}) of {num_parallel_jobs}: using subject indices {start_idx} to {end_idx}.")
         subjects_this_job = subjects[start_idx:end_idx]
         assert len(subjects_this_job) == subjects_count_current_job, f"Expected {subjects_count_current_job} subjects, got {len(subjects_this_job)} at job with index {jobidx}."
         subjects_file_lines = "\n".join(subjects_this_job)
         tfile = f"subjects{jobidx}.txt"
         write_to_textfile(tfile, subjects_file_lines)
-        current_idx = end_idx 
+        current_idx = end_idx
 
     return res
 
@@ -90,6 +103,6 @@ if __name__ == "__main__":
     subjects_file = "subjects.txt"  # input file. we could get these from the command line later.
     num_parallel_jobs_max = 20
     subject_separator : str = ","
-    subjects_txt_to_jobarray_config_sf(subjects_file, num_parallel_jobs_max)
+    split_subjects_txt_into_chunks(subjects_file, num_parallel_jobs_max)
 
 